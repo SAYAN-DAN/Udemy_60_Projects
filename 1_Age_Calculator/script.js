45458cc26568ea2c7a25d64094e9b5a3a8c3347e
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-03-21 20:54:23 +0530

    Add Some Commit

--- 1_Age_Calculator/script.js
+++ 1_Age_Calculator/script.js
@@ -1,4 +1,5 @@
 const birthdayEl = document.querySelector("#birthday");
+const Fresult = document.querySelector("#result");
 document.querySelector("#Btn").addEventListener("click", function () {
   const birthdayValue = birthdayEl.value;
   // console.log(birthdayValue);
@@ -7,12 +8,24 @@ document.querySelector("#Btn").addEventListener("click", function () {
     alert("Please enter a valid date");
   } else {
     const age = getAge(birthdayValue);
+    // console.log(age);
+    Fresult.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
   }
 });
 
 function getAge(birthdayValue) {
   const currentDate = new Date();
   const birthdayDate = new Date(birthdayValue);
-  const age = currentDate.getFullYear() - birthdayDate.getFullYear();
-  console.log(age);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+  // console.log(age);
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
 }
