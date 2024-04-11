069fc32fd1df6c2cd4ff4bbdc938388ffbbf4493
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-04-11 08:29:03 +0530

    commit

--- 5_Pomodoro_Timer/script.js
+++ 5_Pomodoro_Timer/script.js
@@ -3,14 +3,35 @@ const stop = document.querySelector(".stop");
 const reset = document.querySelector(".reset");
 const timer = document.querySelector(".timer");
 
+let interval;
+let timeLeft = 1500;
 start.addEventListener("click", function startTimer() {
-  console.log("start");
+  interval = setInterval(() => {
+    timeLeft--;
+    updatetime();
+    if (timeLeft === 0) {
+      alert("Time's Up");
+      clearInterval(interval);
+      timeLeft = 1500;
+    }
+  }, 1000);
 });
 
 stop.addEventListener("click", function stopTimer() {
-  console.log("stop");
+  clearInterval(interval);
 });
 
 reset.addEventListener("click", function resetTimer() {
-  console.log("reset");
+  clearInterval(interval);
+  timeLeft = 1500;
+  updatetime();
 });
+
+function updatetime() {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+  let formattedtime = `${minutes.toString().padStart(2, "0")} : ${seconds
+    .toString()
+    .padStart(2, "0")}`;
+  timer.innerHTML = formattedtime;
+}
