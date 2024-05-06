d178268d88bc03730e273c4d9cc8247b31a90b55
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-05-06 08:29:56 +0530

    Commit

---
+++ Rock_paper_seissors/script.js
@@ -0,0 +1,10 @@
+
+const buttons = document.querySelectorAll("button")
+const resultEl = document.querySelector("#result")
+
+
+buttons.forEach((button) => {
+    button.addEventListener("click", function() {
+        
+    })
+})
