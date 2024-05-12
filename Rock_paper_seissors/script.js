8692f59207b4b71438e18f6d56bedf9d38cc69f4
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-05-12 07:27:34 +0530

    Commit

--- Rock_paper_seissors/script.js
+++ Rock_paper_seissors/script.js
@@ -1,10 +1,39 @@
+const buttons = document.querySelectorAll("button");
+const resultEl = document.querySelector("#result");
+const playerScoreEl = document.querySelector("#user-score");
+const ComputerScoreEl = document.querySelector("#computer-score");
 
-const buttons = document.querySelectorAll("button")
-const resultEl = document.querySelector("#result")
-
-
+let playerScore = 0;
+let ComputerScore = 0;
 buttons.forEach((button) => {
-    button.addEventListener("click", function() {
-        
-    })
-})
+  button.addEventListener("click", function () {
+    const playerSelection = button.id;
+    const computerSelection = computerplay();
+    const result = playRound(playerSelection, computerSelection);
+    resultEl.textContent = result;
+  });
+});
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It is a tie";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    playerScore++;
+    playerScoreEl.textContent = playerScore;
+    return "You win! " + playerSelection + " beats " + computerSelection;
+  } else {
+    ComputerScore++;
+    ComputerScoreEl.textContent = ComputerScore;
+    return "You lose! " + computerSelection + " beats " + playerSelection;
+  }
+}
+
+function computerplay() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
