3faa1ec77f92f6c6bd113c0e4a54b458279e1c16
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-05-15 16:32:39 +0530

    Commit

---
+++ StopWtach/script.js
@@ -0,0 +1,20 @@
+const timer = document.querySelector("#timer")
+const start = document.querySelector("#Start")
+const stop = document.querySelector("#stop")
+const ReStart = document.querySelector("#ReStart")
+
+
+start.addEventListener("click", function () {
+    console.log("start");
+})
+
+
+stop.addEventListener("click", function () {
+    console.log("stop");
+})
+
+
+ReStart.addEventListener("click", function () {
+    console.log("ReStart");
+});
+
