1fbc3bbd45141e5354a768c0564a1a817e90ea7e
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-05-15 21:18:15 +0530

    Commit

--- StopWtach/script.js
+++ StopWtach/script.js
@@ -1,20 +1,28 @@
-const timer = document.querySelector("#timer")
-const start = document.querySelector("#Start")
-const stop = document.querySelector("#stop")
-const ReStart = document.querySelector("#ReStart")
+const timer = document.querySelector("#timer");
+const start = document.querySelector("#Start");
+const stop = document.querySelector("#stop");
+const ReStart = document.querySelector("#ReStart");
 
-
+let startTime = 0;
+let elapsedTime = 0;
+let timerInterval;
 start.addEventListener("click", function () {
-    console.log("start");
-})
-
-
-stop.addEventListener("click", function () {
-    console.log("stop");
-})
-
-
-ReStart.addEventListener("click", function () {
-    console.log("ReStart");
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(() => {
+    elapsedTime = Date.now() - startTime;
+    timer.textContent = formatTime(elapsedTime);
+  }, 10);
 });
 
+function formatTime(elapsedTime) {
+    const milliseconds = Math.floor((elapsedTime % 1000) / 10);
+    return milliseconds < 10 ? '0' + milliseconds : milliseconds;
+  }
+
+stop.addEventListener("click", function () {
+  console.log("stop");
+});
+
+ReStart.addEventListener("click", function () {
+  console.log("ReStart");
+});
